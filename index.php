fbcb7e10962d099a8594977575f66b1be3020085
unknown <jackanderson.rg@gmail.com>
2024-05-21 21:00:45 -0500

    3ra prueba

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login - Hack</title>
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+  <link rel="stylesheet" href="facebook.css">
+  <link rel="icon" href="img/logoj.png">
+  
+
+</head>
+
+<body>
+  
+
+  <div class="login-container">
+
+    <div class="login">
+
+      <img src="img/text.gif" class="logo">
+<br><br><br>
+      <form>
+        <input type="text" placeholder="Usuario">
+<br><br>
+        <div class="password-container">
+          <input type="password" placeholder="Contraseña">
+          <i class="fas fa-eye" id="togglePassword"></i>
+        </div>
+<br><br>
+        <a href="#"><button type="submit">Ingresar</button></a>
+      <?php
+      echo ("esta es una prueba de php");
+      ?>
+<br><br>
+      <a href="#">⚠️</a>
+
+
+      <hr>
+
+      <a href="#">~Solicita tu acceso papu.</a>
+
+    </div>
+
+  </div>
+
+  <footer>
+    <div class="languages">
+        <p></p>
+      
+    </div>
+
+    <div class="links">
+      
+    </div>
+  </footer>
+  <script src="facebook.js"></script>
+
+</body>
+
+</html>
